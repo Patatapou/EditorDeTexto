cf7a57d70f92245f141259860c509a5258610e07
Patatapou <migel2013@outlook.com>
2025-02-12 12:14:33 -0600

    Agregar archivos de proyecto.

---
+++ EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MennuEditor = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            guardarComoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            rtb = new RichTextBox();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            MennuEditor.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MennuEditor
+            // 
+            MennuEditor.ImageScalingSize = new Size(20, 20);
+            MennuEditor.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem });
+            MennuEditor.Location = new Point(0, 0);
+            MennuEditor.Name = "MennuEditor";
+            MennuEditor.Size = new Size(800, 28);
+            MennuEditor.TabIndex = 2;
+            MennuEditor.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, abrirToolStripMenuItem, guardarToolStripMenuItem, guardarComoToolStripMenuItem, salirToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(73, 24);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(187, 26);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            nuevoToolStripMenuItem.Click += nuevoToolStripMenuItem_Click;
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(187, 26);
+            abrirToolStripMenuItem.Text = "Abrir";
+            abrirToolStripMenuItem.Click += abrirToolStripMenuItem_Click;
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(187, 26);
+            guardarToolStripMenuItem.Text = "Guardar";
+            guardarToolStripMenuItem.Click += guardarToolStripMenuItem_Click;
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            guardarComoToolStripMenuItem.Size = new Size(187, 26);
+            guardarComoToolStripMenuItem.Text = "Guardar como";
+            guardarComoToolStripMenuItem.Click += guardarComoToolStripMenuItem_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(187, 26);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // rtb
+            // 
+            rtb.Location = new Point(0, 31);
+            rtb.Name = "rtb";
+            rtb.Size = new Size(800, 419);
+            rtb.TabIndex = 3;
+            rtb.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(MennuEditor);
+            Controls.Add(rtb);
+            Name = "Form1";
+            Text = "Form1";
+            MennuEditor.ResumeLayout(false);
+            MennuEditor.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip MennuEditor;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private RichTextBox rtb;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ EditorDeTexto/Form1.cs
@@ -0,0 +1,123 @@
+namespace EditorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        string filePath;
+        bool archivoGuardado = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (archivoGuardado == false)
+            {
+                if (MessageBox.Show("Desea crear un nuevo documento y descartar este?", "El archivo no ha sido guardado...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    rtb.Text = "";
+                }
+            }
+            else
+            {
+                guardarToolStripMenuItem_Click(sender, e);
+                rtb.Text = "";
+            }
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = openFileDialog1.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                try
+                {
+                    string texto = File.ReadAllText(filePath);
+                    rtb.Text = texto;
+                    archivoGuardado = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir" + ex.Message);
+                }
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            if (archivoGuardado == false)
+            {
+                resultado = saveFileDialog1.ShowDialog();
+                if (resultado == DialogResult.OK)
+                {
+                    filePath = saveFileDialog1.FileName;
+                    string texto = rtb.Text;
+                    try
+                    {
+                        File.WriteAllText(filePath, texto);
+                        MessageBox.Show("Archivo guardado exitosamente");
+                        archivoGuardado = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al guardar" + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    string texto = rtb.Text;
+                    File.WriteAllText(filePath, texto);
+                    MessageBox.Show("Archivo guardado exitosamente");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar" + ex.Message);
+                }
+            }
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+
+            resultado = saveFileDialog1.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = saveFileDialog1.FileName;
+                string texto = rtb.Text;
+                try
+                {
+                    File.WriteAllText(filePath, texto);
+                    MessageBox.Show("Archivo guardado exitosamente");
+                    archivoGuardado = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar" + ex.Message);
+                }
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (archivoGuardado == false)
+            {
+                if (MessageBox.Show("Desea salir sin guardar?", "El archivo no ha sido guardado...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    this.Dispose();
+                }
+            }
+            else
+            {
+                guardarToolStripMenuItem_Click(sender, e);
+                this.Dispose();
+            }
+        }
+    }
+}
